ac367e6e633d81ad80c26f25496865bfb48971a4
Roman Stadlmair <roman.stadmair@live.com>
2019-04-23 07:54:27 +0200

    Dienstag früh

--- 4-azrgraph.ps1
+++ 4-azrgraph.ps1
@@ -2,19 +2,34 @@
 import-module az.resourcegraph
 get-command -Module az.resourcegraph
 
+# What do i own ?
+Search-AzGraph -Query 'project id'|ft -AutoSize
+
 # Amount of Resources
-$query = "summarize count () by subscriptionId"
-Search-AzGraph -Query $query
+Search-AzGraph -q "summarize count () by subscriptionId"
+Search-AzGraph -q 'summarize count () by resourceGroup |order by count_'
 
-# All with Tag Owner = Roman
-$query = "where tags.Owner=~'Roman'"
-Search-AzGraph -Query $query
-Search-AzGraph -Query $query|select name,tags
+# Easy searching i.e. all with Tag Owner = Roman
+Search-AzGraph -q "where tags.Owner=~'Roman'"|select name,tags
 
-# Same query, do the limitation in the query
-$query = "where tags.Owner=~'Roman'|project name,tags"
-Search-AzGraph -Query $query
+# Same query, do the output limitation in the query
+Search-AzGraph -q "where tags.Owner=~'Roman'|project name,tags"
 
-# Resources with Public IP Addresses
-$Query = "where type contains 'publicIPAddresses' and isnotempty(properties.ipAddress)| summarize count () by subscriptionId"
-Search-AzGraph -Query $query
+# Example:How many resources with public IP addresses are in my tenant ?
+Search-AzGraph -q "where type contains 'publicIPAddresses' and `
+                    isnotempty(properties.ipAddress)| `
+                    summarize count () by subscriptionId"
+
+# What data is provided by ResourceGraph ?
+$query = "where name=~'UbuntuJumpBox'"
+Search-AzGraph -Query $query -outvariable RgVM |out-null
+$RgVM.properties.storageProfile.osDisk.DisksizeGB
+
+#Different Type ==> Same Data
+$query = "where name=~'p-thegalaxy-vnet110'"
+Search-AzGraph -Query $query -OutVariable RGVnet|out-null
+$RGvnet.Properties.addressSpace.AddressPrefixes
+
+# Vague queries allowed
+$query = "where name=~'p-thegalaxy-vnet110'"
+Search-AzGraph -Query $query -OutVariable RGVnet|out-null
